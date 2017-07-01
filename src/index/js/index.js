8031b47fc7a0bb090bfb1badeb7bbf67f45966a9
Apipol Niyomsak <makemek101@hotmail.com>
2017-07-01 23:28:24 +0700

    on login, ajax fetch from server
    DMM Account: /dmm-account
    DMM Session: /dmm-session

--- src/index/js/index.js
+++ src/index/js/index.js
@@ -2,6 +2,11 @@
 
 import { maintenanceCheck } from './kc-maintenance'
 import { initModal } from './modal'
+import { loginByDmmAccount, loginByDmmSession } from './login'
 
 initModal()
 window.onload = () => maintenanceCheck()
+
+// globally accessible
+window.loginByDmmAccount = loginByDmmAccount
+window.loginByDmmSession = loginByDmmSession
